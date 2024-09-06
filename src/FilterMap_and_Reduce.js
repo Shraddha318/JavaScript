fad0823d5e879e0aa3b6b26a03f11137dc229875
Shraddha318 <160100827+Shraddha318@users.noreply.github.com>
2024-09-07 01:46:46 +0530

    commit

--- src/FilterMap_and_Reduce.js
+++ src/FilterMap_and_Reduce.js
@@ -13,10 +13,33 @@ const myNums=[1,2,3,4,5,6,7,8,9]
 // })
 
 //Returning using ForEach
-const nums=[]
-myNums.forEach((num)=>{
-    if(num>4){
-        nums.push(num)
-    }
-})
-console.log(nums)
+// const nums=[]
+// myNums.forEach((num)=>{
+//     if(num>4){
+//         nums.push(num)
+//     }
+// })
+// console.log(nums)
+
+// const nums=[1,2,3,4,5,6,7]
+// // const newNum=nums.map((num)=>{
+// //     return num+10;
+// // })
+// const newNum=nums.map((num)=>num*10).map((num)=>num+10).filter((num)=>num>30)
+// console.log(newNum)
+
+//reduce
+const nums=[1,2,3,4,5,6,7]
+// const myTotal=nums.reduce(function (accumulator,currentvalue){
+//     console.log(`Accumulator ${accumulator} and current value is ${currentvalue}`)
+//     return accumulator+currentvalue;
+// },0)
+
+// const myTotal = nums.reduce((accumulator, currentValue) => {
+//     console.log(`Accumulator ${accumulator} and current value is ${currentValue}`);
+//     return accumulator + currentValue;
+// }, 0);
+const myTotal = nums.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+
+console.log(myTotal)
